5493f50a0c80df96dec1f64ec973cca9982e7329
edwardapaza <edwapazam@upt.pe>
2025-02-13 21:11:36 -0500

    lol

--- src/Financiera.WebApp/Program.cs
+++ src/Financiera.WebApp/Program.cs
@@ -1,8 +1,19 @@
 using Financiera.WebApp.Components;
+using Financiera.WebApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("FinancieraBD")??"";
+builder.Services.AddDbContext<FinancieraContexto>(
+    opt => opt.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+///////////////////////////////
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
